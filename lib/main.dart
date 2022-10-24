176a9762c0398c290e7b5043579e39f08d44f800
carlos <carlos.barahona@ieselcalamot.com>
2022-10-24 16:54:52 +0200

    Examen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:examen_calendari/editar_esdeveniment.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: EditarEsdevenimentForm(),
     );
   }
 }
